c99c3cefcc8f9a6985226a856ae3fd11324eb98e
cpi-aansari <akhataraliansari@gmail.com>
2019-10-08 15:51:03 +0530

    Test Commit

--- TestDevOps/TestDevOps/App.xaml.cs
+++ TestDevOps/TestDevOps/App.xaml.cs
@@ -20,16 +20,19 @@ namespace TestDevOps
         protected override void OnStart()
         {
             // Handle when your app starts
+
         }
 
         protected override void OnSleep()
         {
             // Handle when your app sleeps
+
         }
 
         protected override void OnResume()
         {
             // Handle when your app resumes
+
         }
     }
 }
